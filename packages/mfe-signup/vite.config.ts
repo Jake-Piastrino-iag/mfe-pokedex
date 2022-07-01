f1a646a3020872ec927ebf97cc98ea12c1b2c6b1
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-07-01 22:22:59 +1000

    configure vite module-federation for sign-up package

--- packages/mfe-signup/vite.config.ts
+++ packages/mfe-signup/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+
+import { dependencies, devDependencies } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    federation({
+      name: "mfe-signup",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Signup": "./src/pages/Signup.tsx"
+      },
+      remotes: {},
+      shared: ["react", dependencies]
+    })
+  ]
+});
